c7903e9dc2e9d7795865695f1b04627401f59233
jayraikhere <jayraykhere@gmail.com>
2022-01-04 13:25:22 +0530

    modified scroll-bars in chat

--- src/Components/Mainpage/Mainpage.js
+++ src/Components/Mainpage/Mainpage.js
@@ -19,125 +19,134 @@ import Setting from "./Setting";
 const drawerWidth = "360";
 
 const Main = styled("main", { shouldForwardProp: (prop) => prop !== "open" })(
-  ({ theme, open }) => ({
-    flexGrow: 1,
-    padding: theme.spacing(3),
-    height: "85vh",
-    transition: theme.transitions.create("margin", {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-    ...(open && {
-      paddingRight: `${parseInt(drawerWidth) + 10}px`,
-      transition: theme.transitions.create("margin", {
-        easing: theme.transitions.easing.easeOut,
-        duration: theme.transitions.duration.enteringScreen,
-      }),
-      marginRight: 0,
-    }),
-  })
+	({ theme, open }) => ({
+		flexGrow: 1,
+		padding: theme.spacing(3),
+		height: "85vh",
+		transition: theme.transitions.create("margin", {
+			easing: theme.transitions.easing.sharp,
+			duration: theme.transitions.duration.leavingScreen,
+		}),
+		...(open && {
+			paddingRight: `${parseInt(drawerWidth) + 10}px`,
+			transition: theme.transitions.create("margin", {
+				easing: theme.transitions.easing.easeOut,
+				duration: theme.transitions.duration.enteringScreen,
+			}),
+			marginRight: 0,
+		}),
+	})
 );
 
 const Mainpage = () => {
-  const [loading, setLoading] = useState(false);
-  const [url, seturl] = useState("https://www.youtube.com/watch?v=jNC7hJUMmHU");
-  const [open, setopen] = useState(true);
+	const [loading, setLoading] = useState(false);
+	const [url, seturl] = useState(
+		"https://www.youtube.com/watch?v=jNC7hJUMmHU"
+	);
+	const [open, setopen] = useState(true);
 
-  const handleClick = () => {
-    setLoading(true);
-  };
+	const handleClick = () => {
+		setLoading(true);
+	};
 
-  const onchange = (e) => {
-    seturl(e.target.value);
-  };
+	const onchange = (e) => {
+		seturl(e.target.value);
+	};
 
-  const toggleSidebar = () => {
-    setopen(!open);
-  };
+	const toggleSidebar = () => {
+		setopen(!open);
+	};
 
-  return (
-    <>
-      <Box style={{ minHeight: "100vh", minWidth: "100vw" }}>
-        <Box spacing={1}>
-          <Box style={{ display: "flex" }}>
-            <TextField
-              label="Video Url"
-              sx={{
-                width: "70vw",
-                margin: "10px",
-                backgroundColor: "rgba(20,20,35,0.4)",
-              }}
-            />
-            <LoadingButton
-              onClick={handleClick}
-              loading={loading}
-              loadingPosition="end"
-              variant="contained"
-              sx={{ width: "23vw", margin: "10px" }}
-            >
-              Play
-            </LoadingButton>
-            <Button>
-              <IconButton
-                color="inherit"
-                aria-label="open drawer"
-                onClick={toggleSidebar}
-              >
-                <MenuIcon />
-              </IconButton>
-            </Button>
-          </Box>
-          <Box spacing={1}>
-            <Main open={open}>
-              <ReactPlayer url={url} height={"100%"} width={"100%"} />
-            </Main>
-            <Drawer
-              sx={{
-                width: `${drawerWidth}px`,
-                flexShrink: 0,
-                "& .MuiDrawer-paper": {
-                  width: `${drawerWidth}px`,
-                },
-              }}
-              variant="persistent"
-              anchor="right"
-              open={open}
-            >
-              <IconButton onClick={() => setopen(false)}>
-                <ChevronRightIcon />
-              </IconButton>
-              <div
-                style={{
-                  width: "95%",
-                  margin: "10px",
-                  backgroundColor: "white",
-                  padding: "4px",
-                  fontFamily: "'Baloo Tammudu 2', cursive",
-                  fontSize: "1.3em",
-                }}
-              >
-                {"Jay Raikhere"}
-              </div>
-              <Divider />
-              <Grid container spacing={1}>
-                <Grid item xs={4}>
-                  <Button variant="secondary">Chat</Button>
-                </Grid>
-                <Grid item xs={4}>
-                  <Button variant="secondary">People</Button>
-                </Grid>
-                <Grid item xs={4}>
-                  <Button variant="secondary">Settings</Button>
-                </Grid>
-              </Grid>
-              <Divider />
-              <Chat />
-            </Drawer>
-          </Box>
-        </Box>
-      </Box>
-    </>
-  );
+	return (
+		<>
+			<Box style={{ minHeight: "100vh", minWidth: "100vw" }}>
+				<Box spacing={1}>
+					<Box style={{ display: "flex" }}>
+						<TextField
+							label="Video Url"
+							sx={{
+								width: "70vw",
+								margin: "10px",
+								backgroundColor: "rgba(20,20,35,0.4)",
+							}}
+						/>
+						<LoadingButton
+							onClick={handleClick}
+							loading={loading}
+							loadingPosition="end"
+							variant="contained"
+							sx={{ width: "23vw", margin: "10px" }}
+						>
+							Play
+						</LoadingButton>
+						<Button>
+							<IconButton
+								color="inherit"
+								aria-label="open drawer"
+								onClick={toggleSidebar}
+							>
+								<MenuIcon />
+							</IconButton>
+						</Button>
+					</Box>
+					<Box spacing={1}>
+						<Main open={open}>
+							<ReactPlayer
+								url={url}
+								height={"100%"}
+								width={"100%"}
+							/>
+						</Main>
+						<Drawer
+							sx={{
+								width: `${drawerWidth}px`,
+								flexShrink: 0,
+								"& .MuiDrawer-paper": {
+                  overflowX: "hidden",
+									width: `${drawerWidth}px`,
+								},
+							}}
+							variant="persistent"
+							anchor="right"
+							open={open}
+						>
+							<IconButton onClick={() => setopen(false)}>
+								<ChevronRightIcon />
+							</IconButton>
+							<div
+								style={{
+									width: "95%",
+									margin: "10px",
+									backgroundColor: "white",
+									padding: "4px",
+									fontFamily: "'Baloo Tammudu 2', cursive",
+									fontSize: "1.3em",
+								}}
+							>
+								{"Jay Raikhere"}
+							</div>
+							<Divider />
+							<Grid container spacing={1}>
+								<Grid item xs={4}>
+									<Button variant="secondary">Chat</Button>
+								</Grid>
+								<Grid item xs={4}>
+									<Button variant="secondary">People</Button>
+								</Grid>
+								<Grid item xs={4}>
+									<Button variant="secondary">
+										Settings
+									</Button>
+								</Grid>
+							</Grid>
+							<Divider />
+							<Chat />
+						</Drawer>
+					</Box>
+				</Box>
+			</Box>
+		</>
+	);
 };
 
 export default Mainpage;
