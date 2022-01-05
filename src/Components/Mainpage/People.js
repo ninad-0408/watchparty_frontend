b6fdd9038f59d2e0f3f2ba0ed5dec5e6eb2c4ad2
jayraikhere <jayraykhere@gmail.com>
2022-01-05 19:36:51 +0530

    edit People.js

--- src/Components/Mainpage/People.js
+++ src/Components/Mainpage/People.js
@@ -1,6 +1,14 @@
 import React from "react";
 import { styled } from "@mui/material/styles";
 import Paper from "@mui/material/Paper";
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import AdminPanelSettingsIcon from '@mui/icons-material/AdminPanelSettings';
+import CancelPresentationIcon from '@mui/icons-material/CancelPresentation';
+import ToggleButton from '@mui/material/ToggleButton';
+import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
+import Grid from '@mui/material/Grid';
 
 const People = () => {
 	const Item = styled(Paper)(({ theme }) => ({
@@ -9,15 +17,51 @@ const People = () => {
 		textAlign: "center",
 		color: theme.palette.text.secondary,
 	}));
+	
+	const arr=[{name:'john'},{name:'john john'},{name:'john john john'},{name:'john john john john'},{name:'john'}, {name:'john'}, {name:'john'} ,{name:'john'}]
 
 	return (
 		<div>
-			<Item>user: Hello</Item>
-			<Item>user: Hello</Item>
-			<Item>user: Hello</Item>
-			<Item>user: Hello</Item>
+			{arr.map((data)=>{
+				return <Item>{Cards({name:data.name})}</Item>
+			})}
+			{/* <Item>{Cards({name:'hello'})}</Item> */}
 		</div>
 	);
 };
+
+const Cards=(props)=>{
+	const [alignment, setAlignment] = React.useState('web');
+	const [selected, setSelected] = React.useState(false);
+
+  const handleChange = (event, newAlignment) => {
+    setAlignment(newAlignment);
+	setSelected(!selected);
+  };
+	return(<Card sx={{ minWidth: 275 }}>
+	<CardContent>
+	<Grid container spacing={2}>
+      <Grid item xs={8}>
+	  <Typography variant="h5" component="span">
+		{props.name + '  '}
+	  </Typography>
+	  </Grid>
+	  <Grid item xs={4}>
+	  <ToggleButtonGroup
+      color="primary"
+      exclusive
+	  value={alignment}
+      selected={selected}
+      onChange={handleChange}
+    >
+      
+	  <ToggleButton value="abc"><AdminPanelSettingsIcon/></ToggleButton>
+      <ToggleButton value="ios"><CancelPresentationIcon/></ToggleButton>
+    </ToggleButtonGroup>
+	</Grid>
+	</Grid>
+	</CardContent>
+	
+  </Card>)}
 
 export default People;
