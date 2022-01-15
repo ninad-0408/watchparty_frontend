bc9847691f180ac04ad10ba7710fdbb29bb1ffe6
Ninad Kalanke <ninadkalanke@gmail.com>
2022-01-15 22:24:39 +0530

    player set admin controls chalu

--- src/Components/Mainpage/Mainpage.js
+++ src/Components/Mainpage/Mainpage.js
@@ -41,26 +41,24 @@ const Main = styled("main", { shouldForwardProp: (prop) => prop !== "open" })(
 
 const Mainpage = ({ socket }) => {
   const { roomId } = useParams();
-  const [loading, setLoading] = useState(false);
   const [url, seturl] = useState("");
   const [open, setopen] = useState(true);
 
   const [members, setmembers] = useState([]);
   const [message, setMessage] = useState([]);
 
-  const [progress, setProgress] = useState();
 	const [playing, setplaying] = useState(true);
 	const player = useRef(null);
 
   const username = JSON.parse(localStorage.getItem("profile"))?.user?.username;
-
-  const handleClick = () => {
-    setLoading(true);
-  };
+  const [user, setuser] = useState(null);
 
   const onchange = (e) => {
-    socket.emit("url", {roomId, url: e.target.value});
-    seturl(e.target.value);
+    if(user.isAdmin)
+    {
+      socket.emit("url", { roomId, url: e.target.value});
+      seturl(e.target.value);
+    }
   };
 
   const toggleSidebar = () => {
@@ -73,21 +71,22 @@ const Mainpage = ({ socket }) => {
     setValue(newValue);
   };
 
-  const [streaming, setStreaming] = useState();
-	const myVideo = useRef();
-	const userVideo = useRef();
+  // const [streaming, setStreaming] = useState();
+	// const myVideo = useRef();
+	// const userVideo = useRef();
 
   useEffect(() => {
-    socket.on("connect", () => {
-      console.log("connected", socket);
-      socket.emit("new-member", roomId);
-    });
+    socket.emit("new-member", roomId);
+  },[]);
+
+  useEffect(() => {
 
     socket.on("message", (mess) => {
       setMessage((prev) => [...prev, mess]);
     });
 
     socket.on("member-connected", (member) => {
+      setuser(member.find((mem) => mem.username === username));
       setmembers(() => [...member]);
     });
 
@@ -97,67 +96,73 @@ const Mainpage = ({ socket }) => {
 
 		socket.on("seek", (data) => {
 			if (data.pause) {
-				setProgress(data.seek);
 				player.current.seekTo(data.seek, "seconds");
 				setplaying(false);
 			} else setplaying(true);
 		});
 
-		socket.on("stream", (stream) => {
-				myVideo.current.srcObject = stream;
-			});
+		// socket.on("stream", (stream) => {
+		// 		myVideo.current.srcObject = stream;
+		// 	});
 
   }, [socket]);
 
-  // const [progress, setProgress] = useState();
-  // const player = useRef(null);
-
   const seek = () => {
-		socket.emit("seek", {
-      roomId: roomId,
-			seek: progress,
-			pause: false,
-		});
-		player.current.seekTo(progress, "seconds");
+    if(user.isAdmin)
+    {
+      var currentTime = player.current.getCurrentTime();
+		  socket.emit("seek", {
+        roomId: roomId,
+			  seek: currentTime,
+        pause: !playing
+		  });
+    }
 	};
 
   const pause = () => {
-		var currentTime = player.current.getCurrentTime();
-		socket.emit("seek", {
-			roomId: roomId,
-			seek: currentTime,
-			pause: true,
-		});
+    if(user.isAdmin)
+		{
+      var currentTime = player.current.getCurrentTime();
+		  socket.emit("seek", {
+			  roomId: roomId,
+			  seek: currentTime,
+			  pause: true,
+		  });
+    }
 	};
+
 	const play = () => {
-		var currentTime = player.current.getCurrentTime();
-		socket.emit("seek", {
-			roomId: roomId,
-			seek: progress,
-			pause: false,
-		});
+    if(user.isAdmin)
+		{
+      var currentTime = player.current.getCurrentTime();
+		  socket.emit("seek", {
+			  roomId: roomId,
+			  seek: currentTime,
+			  pause: false,
+		  });
+    }
 	};
 
 
-	const [file, setfile] = useState(null);
-	const [media, setmedia] = useState(null);
+	// const [file, setfile] = useState(null);
+	// const [media, setmedia] = useState(null);
 
-	const upload = (e) => {
-		seturl(URL.createObjectURL(e.target.files[0]));
-		player.current.play();
-	};
+	// const upload = (e) => {
+	// 	seturl(URL.createObjectURL(e.target.files[0]));
+	// 	player.current.play();
+	// };
 
-  const mediastream = ()=>{navigator.mediaDevices.getDisplayMedia({audio:true,video:true}).then((currentStream) => {
-    setStreaming(currentStream);
-    myVideo.current.srcObject = currentStream;
-    socket.emit("stream", {currentStream})
-  })}
+  // const mediastream = ()=>{navigator.mediaDevices.getDisplayMedia({audio:true,video:true}).then((currentStream) => {
+  //   setStreaming(currentStream);
+  //   myVideo.current.srcObject = currentStream;
+  //   socket.emit("stream", {currentStream})
+  // })}
 
-const camstream = ()=>{navigator.mediaDevices.getUserMedia({audio:true,video:true}).then((currentStream) => {
-    setStreaming(currentStream);
-    myVideo.current.srcObject = currentStream;
-    socket.emit("stream",currentStream)
-  })}
+  // const camstream = ()=>{navigator.mediaDevices.getUserMedia({audio:true,video:true}).then((currentStream) => {
+  //   setStreaming(currentStream);
+  //   myVideo.current.srcObject = currentStream;
+  //   socket.emit("stream",currentStream)
+  // })}
 
   return (
     <>
@@ -176,7 +181,6 @@ const camstream = ()=>{navigator.mediaDevices.getUserMedia({audio:true,video:tru
             />
             <LoadingButton
               onClick={seek}
-              loading={loading}
               loadingPosition="end"
               variant="contained"
               sx={{ width: "23vw", margin: "10px" }}
@@ -201,17 +205,10 @@ const camstream = ()=>{navigator.mediaDevices.getUserMedia({audio:true,video:tru
                 width={"100%"}
                 playing={playing}
 								controls={true}
-								volume={0.9}
-								start={15}
-								rel={0}
 								ref={player}
 								onPause={pause}
-								onPlay={() => play()}
-								onSeek={pause}
-								onProgress={(e) => {
-									// console.log(progress);
-									setProgress(e.playedSeconds);
-								}}
+								onPlay={play}
+								onSeek={seek}
               />
             </Main>
             <Drawer
@@ -263,11 +260,11 @@ const camstream = ()=>{navigator.mediaDevices.getUserMedia({audio:true,video:tru
           </Box>
         </Box>
       </Box>
-      <Button onClick = {()=>mediastream()}>screen-share</Button>
+      {/* <Button onClick = {()=>mediastream()}>screen-share</Button>
 			<Button onClick = {()=>camstream()}>Camera</Button>
 			<input type="file" accept='video/*' id="myfile" onChange={upload} />
 			<video playsInline muted ref={myVideo} autoPlay  />
-			<video playsInline muted ref={userVideo} autoPlay />
+			<video playsInline muted ref={userVideo} autoPlay /> */}
 
     </>
   );
