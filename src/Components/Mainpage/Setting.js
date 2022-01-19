b595242eaaa7b657a8daf90a105d6ad15b32a826
Ninad Kalanke <ninadkalanke@gmail.com>
2022-01-19 20:32:31 +0530

    Delete room and close room functionality added

--- src/Components/Mainpage/Setting.js
+++ src/Components/Mainpage/Setting.js
@@ -1,39 +1,66 @@
 import React, { useState } from "react";
-import Stack from "@mui/material/Stack";
 import Switch from "@mui/material/Switch";
+import { LoadingButton } from "@mui/lab";
+import { Grid } from "@mui/material";
+import { delRoom } from "../../Api";
 
 const Setting = ({ room, currentuser, socket }) => {
-
   const [loading, setloading] = useState(false);
+  const [loading1, setloading1] = useState(false);
 
   const handleChange = () => {
-    if(currentuser.isHost)
-    {
+    if (currentuser.isHost) {
       setloading(true);
-      socket.emit('lock-room', room._id, !room.lock);
+      socket.emit("close-room", room._id);
     }
-  }
+  };
+
+  const handleChange1 = () => {
+    if (currentuser.isHost) {
+      setloading1(true);
+      socket.emit("close-room", room._id);
+      delRoom(room._id);
+    }
+  };
+
+  const handleChange2 = () => {
+    if (currentuser.isHost) socket.emit("lock-room", room._id, !room.lock);
+  };
 
   return (
-    <div>
-      <Stack
-        spacing={0.5}
-        sx={{ width: "95%", margin: "10px", height: "60vh", padding: "0px" }}
-        style={{ overflowY: "scroll", "white-space": "nowrap" }}
-      >
-        <div
-          sx={{
-            width: "20vw",
-            backgroundColor: "rgb(167,149,96)",
-            margin: "10px",
-          }}
-        >
-          Lock Room
-          <Switch checked={room.lock} loading={loading} onClick={handleChange} />
-        </div>
-      </Stack>
-    </div>
+    <Grid container margin={3} spacing={3}>
+      <Grid item sx={6}>
+        Lock Room
+        <Switch checked={room.lock} onClick={handleChange2} />
+      </Grid>
+      {currentuser.isHost ? (
+        <>
+          <Grid item sx={6}>
+            <LoadingButton
+              loading={loading}
+              color="warning"
+              variant="contained"
+              onClick={handleChange}
+            >
+              Close Room
+            </LoadingButton>
+          </Grid>
+          <Grid item sx={12} marginTop={2} marginLeft={19}>
+            <LoadingButton
+              loading={loading1}
+              color="error"
+              variant="contained"
+              onClick={handleChange1}
+            >
+              Delete Room
+            </LoadingButton>
+          </Grid>
+        </>
+      ) : (
+        <> </>
+      )}
+    </Grid>
   );
-}
+};
 
 export default Setting;
